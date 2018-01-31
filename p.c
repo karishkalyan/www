d1dfec6c0328469ef58358091bbb1f8c3727a75e
karishkalyan <35295068+karishkalyan@users.noreply.github.com>
2018-01-31 16:54:15 +0530

    power
    to calculate the power

---
+++ p.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int n,m,p;
+printf("\n enter the numbers:");
+scanf("%d,%d",&n,&m);
+p=pow(n,m);
+printf("\n the power of the num is %d ",p);
+getch();
+}
